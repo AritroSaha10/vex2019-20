470414fbb09a2ce50c9e5f0385371bc60781aa8a
RayhaanT <tanweer.rayhaan@gmail.com>
2019-09-30 21:32:17 -0400

    Basic controls test

--- V5/include/main.h
+++ V5/include/main.h
@@ -39,7 +39,7 @@
 /**
  * You should add more #includes here
  */
-//#include "okapi/api.hpp"
+#include "okapi/api.hpp"
 //#include "pros/api_legacy.h"
 
 /**
@@ -62,6 +62,7 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+
 void autonomous(void);
 void initialize(void);
 void disabled(void);
--- V5/src/opcontrol.cpp
+++ V5/src/opcontrol.cpp
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <initializer_list>
 
 /**
  * Runs the operator control code. This function will be started in its own task
@@ -13,19 +14,57 @@
  * operator control task will be stopped. Re-enabling the robot will restart the
  * task, not resume it from where it left off.
  */
+
+using namespace pros;
+using namespace okapi;
+
+pros::Motor frontLeftMtr (1);
+pros::Motor backLeftMtr (2);
+pros::Motor frontRightMtr (3);
+pros::Motor backRightMtr (4);
+
+auto drive = ChassisControllerFactory::create(
+	{1, 2}, {3, 4},
+	AbstractMotor::gearset::green,
+	{4_in, 11.5_in}
+);
+
 void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::Motor left_mtr(1);
-	pros::Motor right_mtr(2);
-	while (true) {
+
+	//TRADITIONAL CONTROLLER
+	//pros::Controller master(pros::E_CONTROLLER_MASTER);
+
+	//OKAPILIB CONTROLLER
+	okapi::Controller master;
+
+		while (true)
+	{
 		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
 		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
 		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
+
+		/*Traditional
 		int left = master.get_analog(ANALOG_LEFT_Y);
 		int right = master.get_analog(ANALOG_RIGHT_Y);
 
-		left_mtr = left;
-		right_mtr = right;
+		backLeftMtr = left;
+		frontLeftMtr = left;
+		frontRightMtr = right;
+		backRightMtr = right;
+
 		pros::delay(20);
+		*/
+
+		//OkapiLib
+
+		drive.tank(master.getAnalog(ControllerAnalog::leftY),
+				   master.getAnalog(ControllerAnalog::rightY));
+
+		/*OkapiLib Arcade
+		drive.arcade(master.getAnalog(ControllerAnalog::leftY),
+					master.getAnalog(ControllerAnalog::rightY));
+		*/
+
+		pros::delay(10);
 	}
 }
