c3d3c96cff6defdce580c64636366aa44bfc750a
RayhaanT <tanweer.rayhaan@gmail.com>
2019-11-20 17:50:18 -0500

    Fixed chassis indexing errors

--- V5/include/chassis.h
+++ V5/include/chassis.h
@@ -2,16 +2,30 @@
 #include "main.h"
 #include "vector"
 
-#define FL 1
-#define FR 2
-#define BL 9
-#define BR 10
+// allMotors array index --- Drive Motors
+#define FL 0
+#define FR 1
+#define BL 2
+#define BR 3
+
+// Physical motor port connections -- Drive Motors
+#define FL_PORT 1
+#define FR_PORT 2
+#define BL_PORT 9
+#define BR_PORT 10
 
 // ---------------- PLACEHOLDERS --------------------//
-#define M1 3
-#define M2 4
-#define M3 7
-#define M4 8
+// allMotors array index --- Misc. Motors
+#define M1 4
+#define M2 5
+#define M3 6
+#define M4 7
+
+// Physical motor port connections -- Misc. Motors
+#define M1_PORT 3
+#define M2_PORT 4
+#define M3_PORT 7
+#define M4_PORT 8
 // ---------------- PLACEHOLDERS --------------------//
 
 double joystickSlew(double input);
