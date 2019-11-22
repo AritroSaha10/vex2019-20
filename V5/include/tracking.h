b9598cf56ccef9df5ab3fbb8a515175803191a11
itsarune <arunbsfc1@outlook.com>
2019-11-21 22:49:03 -0500

    Started implemented tracking (without accounting for drift)

--- V5/include/tracking.h
+++ V5/include/tracking.h
@@ -4,6 +4,7 @@
 #include "tracking.h"
 #include "main.h"
 #include "chassis.h" 
+#define _USE_MATH_DEFINES
 
 /*
 	Tracks encoder positions; No parameters
--- V5/src/tracking.cpp
+++ V5/src/tracking.cpp
@@ -1,15 +1,27 @@
 #include "tracking.h"
 #include "chassis.h"
 
-double lDist, rDist;
+#define DEGREE_TO_CM M_PI*8.255/360 //cm per tick
+
+double lDist, rDist, dist, deltaL, deltaR, deltaS = 0;
+double lastL, lastR, lastS, lastTheta = 0;
+double theta, distOfWheels, changeInTheta, x, y;
 
 void tracking(void* param) {
 	while(true) {
+		
 		std::vector<double> encoders = getEncoders({FL, FR});
-		lDist = encoders[0];
-		rDist = encoders[1];
-		std::cout << "Left Distance " << lDist;
-		std::cout << "Right Distance " << rDist;
+		lDist = (encoders[0]-lDist)*DEGREE_TO_CM;
+		rDist = encoders[1]-rDist*DEGREE_TO_CM;
+		dist = (rDist+lDist)/2;
+		theta = (deltaL-deltaR)/distOfWheels; 
+		changeInTheta = theta-lastTheta;
+
+		lastL = lDist;
+		lastR = rDist;
+		lastTheta = theta;
+		pros::lcd::print(2, "deltaL %f", deltaL);
+		pros::lcd::print(3, "deltaR %f", deltaR);
 		pros::delay(2);
 	}
 }
