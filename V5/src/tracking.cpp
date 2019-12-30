31746bc821ff47d702158089200780d6459f185e
RayhaanT <tanweer.rayhaan@gmail.com>
2019-12-30 15:39:56 -0500

    PDF-tracking

--- V5/src/tracking.cpp
+++ V5/src/tracking.cpp
@@ -3,37 +3,50 @@
 #define _USE_MATH_DEFINES
 #include <math.h>
 
-	//cm per tick
-
-float lDist, rDist, dist, lastTheta, x, y, rCycle, lCycle = 0;
-float theta;
-float changeInTheta;
-float distOfWheels = 26.6; 		//cm
+//Globals
+float x, y, angle;
+//Sensors
+float rDelta, lDelta, bDelta;
 std::vector<double> encoders;
+//Temp
+float rDist, lDist, aDelta, rLast, lLast;
+//constants
+const float lrOffset = 13/2*2.54; //13 inch wheel base, halved and converted to cm
+const float bOffset = 0; /*PLACEHOLDER*/
 
 void tracking(void* param) {
+	float localCoord[2];
 	while(1) {
-		
-		encoders = getEncoders({FL, FR});
-		lCycle = ((encoders[0])*DEGREE_TO_CM)-lDist;
-		rCycle = ((encoders[1])*DEGREE_TO_CM)-rDist;
-		rDist+= rCycle;
-		lDist += lCycle;	
-		
-		dist = (rCycle+lCycle)/2;
-		theta = (rDist-lDist)/distOfWheels; 
-		changeInTheta = (theta - lastTheta);
-		if (abs((theta)-(lastTheta)) >= 0.001) {
-			x += dist*cos(changeInTheta);
-			y += dist*sin(changeInTheta);
-		}	
+		localCoord[0] = 0;
+		localCoord[1] = 0;
+		std::vector<double> rawEncoders = getEncoders({FL, BL, FR, BR});
+		encoders.push_back((rawEncoders[1] + rawEncoders[0])/2);
+		encoders.push_back((rawEncoders[2] + rawEncoders[3])/2);
 
-		pros::lcd::print(2, "x: %f, y: %f, theta: %f", x, y, theta);
-		pros::lcd::print(3, "theta: %f, lastTheta: %f", theta, lastTheta);
-		pros::lcd::print(4, "change in theta: %f", (theta-lastTheta));
-		lastTheta = theta;
-		rDist = encoders[0]*DEGREE_TO_CM;
-		lDist = encoders[1]*DEGREE_TO_CM;
-		pros::delay(2);
+		lDelta = encoders[0]-lLast;
+		rDelta = encoders[1]-rLast;
+		lLast = encoders[0];
+		rLast = encoders[1];
+		lDist = lDelta * DEGREE_TO_CM;
+		rDist = rDelta * DEGREE_TO_CM;
+
+		if(rDelta == lDelta) {
+			aDelta = 0;
+			localCoord[1] = rDist;
+		}
+		if(lDelta > rDelta) {
+			aDelta = (lDist-rDist)/(lrOffset*2);
+			localCoord[1] = (2*sin(aDelta/2))*(rDist/aDelta+lrOffset);
+		}
+		else {
+			aDelta = (rDist-lDist)/(lrOffset*2);
+			localCoord[1] = (2*sin(aDelta/2))*(lDist/aDelta+lrOffset);
+		}
+		float localOffset = angle + (aDelta/2);
+		x += localCoord[0]*cos(localOffset) - localCoord[1]*sin(localOffset);
+		y += localCoord[0]*sin(localOffset) + localCoord[1]*cos(localOffset);
+
+		angle += aDelta;
+		pros::lcd::print(2, "x: %f, y: %f, theta: %f", x, y, angle);
 	}
 }
