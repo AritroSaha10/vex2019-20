53e095c87530ae9e29e809465cedf6b21c25c452
RayhaanT <tanweer.rayhaan@gmail.com>
2020-02-01 14:23:55 -0500

    Memory

---
+++ V5/src/globals.cpp
@@ -0,0 +1,26 @@
+#include "globals.h"
+#include "chassis.h"
+
+okapi::Controller master;
+
+Intake intake(0x13, master);
+Tray tray(Tray::IDLE_STATE, master, intake);
+Lift lift(0x10, tray);
+
+pros::Motor frontLeftDrive(FL_PORT, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor backLeftDrive(BL_PORT, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor frontRightDrive(FR_PORT, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor backRightDrive(BR_PORT, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor lift_motor(LIFT_PORT, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor tray_motor(TRAY_PORT, pros::E_MOTOR_GEARSET_36, true, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor rightIntake_motor(RINTAKE_PORT, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor leftIntake_motor(LINTAKE_PORT, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES);
+
+void nullTask(void *param)
+{
+    pros::delay(10);
+}
+pros::Task update(nullTask, (void *)"PROS", TASK_PRIORITY_DEFAULT, TASK_STACK_DEPTH_DEFAULT, "null");
+
+// pros::ADIPotentiometer trayPot(TRAY_POT_PORT);
+// pros::ADIPotentiometer liftPot(LIFT_POT_PORT);
